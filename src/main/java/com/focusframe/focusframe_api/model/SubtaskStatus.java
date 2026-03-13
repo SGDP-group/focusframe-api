de9be147a685a6ac62cc44550395e009c4355c87
rizlan - uni <rizlan.20223256@iit.ac.lk>
2026-03-13 23:49:28 +0530

    removed getter and Setters

--- src/main/java/com/focusframe/focusframe_api/model/SubtaskStatus.java
+++ src/main/java/com/focusframe/focusframe_api/model/SubtaskStatus.java
@@ -22,24 +22,5 @@ public class SubtaskStatus {
     private String name;
     
 
-    
-    public SubtaskStatus(String name) {
-        this.name = name;
-    }
-    
-    public Integer getId() {
-        return id;
-    }
-    
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    
-    public String getName() {
-        return name;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
+
 }
