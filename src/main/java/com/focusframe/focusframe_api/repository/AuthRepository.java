a1e6a41a82f55f8791345f222b4660f22a58e4b4
Jason Anthony <sethsuna333@gmail.com>
2026-03-22 07:18:41 +0530

    implementing method at repository level

--- src/main/java/com/focusframe/focusframe_api/repository/AuthRepository.java
+++ src/main/java/com/focusframe/focusframe_api/repository/AuthRepository.java
@@ -1,7 +1,11 @@
 package com.focusframe.focusframe_api.repository;
 
 import com.focusframe.focusframe_api.model.AuthToken;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +14,9 @@ import java.util.Optional;
 public interface AuthRepository extends JpaRepository<AuthToken, Long> {
     Optional<AuthToken> findByToken(String token);
     Optional<AuthToken> findByIpAddress(String ipAddress);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE auth_tokens SET expired_status = 1 WHERE token = :token", nativeQuery = true)
+    void setAuthAsExpired(@Param("token") String token);
 }
